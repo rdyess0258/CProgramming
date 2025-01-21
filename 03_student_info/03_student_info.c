3ddeef10c3769f015404629649917d76e8c30b4f
rdyess0258 <rdyess0258@floridapoly.edu>
2025-01-21 18:16:04 +0000

    complete

---
+++ 03_student_info/03_student_info.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int course_id = 2271;
+    int student_id = 10234;
+
+    printf("Name: Rivers Dyess\n ");
+    printf("Course ID: %d\n", course_id);
+    printf("Student ID: %d\n", student_id);
+    printf("Student ID - Course ID = %d\n", student_id - course_id);
+
+    return 0;
+
+}
